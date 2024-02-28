1e56082975e4c1c6b3b1c32559e407a648836713
Ikem <ikempeter2020@gmail.com>
2024-02-28 13:08:59 +0100

    added  global setting model

--- src/helpers/server/db/chatbotModel.js
+++ src/helpers/server/db/chatbotModel.js
@@ -2,23 +2,12 @@ import mongoose from 'mongoose';
 
 const Schema = mongoose.Schema;
 
-export default function userModel() {
+export default function chatbotModel() {
     const schema = new Schema({
-        email: { type: String, unique: true, required: true },
-        hash: { type: String, required: true },
-        googleId: { type: String, }
-    }, {
-        timestamps: true
-    });
+        botId: { type: String, unique: true, required: true },
+        pIndex: { type: String, required: true },
+        model: { type: String, required: true}
+    }, { timestamps: true});
 
-    schema.set('toJSON', {
-        virtuals: true,
-        versionKey: false,
-        transform: function (doc, ret) {
-            delete ret._id;
-            delete ret.hash;
-        }
-    });
-    schema.index({ googleId: 1 }, { unique: true, sparse: true })
-    return mongoose.models.User || mongoose.model('User', schema);
+    return mongoose.models.Chatbot || mongoose.model('Chatbot', schema);
 }
