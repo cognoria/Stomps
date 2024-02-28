5a573028912bf5f2d373bfed489694b5d20264de
Ikem <ikempeter2020@gmail.com>
2024-02-28 14:51:03 +0100

    knowledge base

--- src/helpers/server/db/logModel.js
+++ src/helpers/server/db/logModel.js
@@ -5,7 +5,7 @@ const Schema = mongoose.Schema;
 export default function logModel() {
 
     const schema = new Schema({
-        userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true, },
+        user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true, },
         action: { type: String, required: true, },
         timestamp: { type: Date, default: Date.now, },
         details: mongoose.Schema.Types.Mixed,
