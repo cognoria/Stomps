39981051f85106774b061f48ec79544c9e9a3f36
Ikem <ikempeter2020@gmail.com>
2024-03-06 10:20:20 +0100

    restructure FE to match reset url backend

---
+++ src/app/(app)/(auth)/reset/[token]/page.js
@@ -0,0 +1,16 @@
+import Auth_header from "../../../../../components/authComponents/header";
+import Reset_password from "../../../../../components/authComponents/reset_password/reset_password";
+
+function Page({ token }) {
+  return (
+    <div className=" overflow-x-hidden">
+      <Auth_header
+        desc={"Reset your password"}
+        desc2={"Your new password should be easy to recall  "}
+      />
+      <Reset_password token={token} />
+    </div>
+  );
+}
+
+export default Page;
