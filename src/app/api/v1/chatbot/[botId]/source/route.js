03af1c668df14ba8a012e1a86ecf7cf12aa8c52d
Ikem <ikempeter2020@gmail.com>
2024-04-08 11:20:21 +0100

    fixes

--- src/app/api/v1/chatbot/[botId]/source/route.js
+++ src/app/api/v1/chatbot/[botId]/source/route.js
@@ -5,6 +5,7 @@ import { apiHandler } from "../../../../../../helpers/server/api";
 
 module.exports = apiHandler({
     POST: updateSource,
+    GET: getSource,
 });
 
 //route POST api/v1/chatbot/[botId]/source
@@ -12,6 +13,12 @@ async function updateSource(req) {
     const { botId } = req.params;
     const params = await req.json()
     return await chatbotRepo.updateKnowledgebase(botId, params);
+}
+
+//route GET api/v1/chatbot/[botId]/source
+async function getSource(req) {
+    const { botId } = req.params;
+    return await chatbotRepo.getChatbotSources(botId);
 }
 
 updateSource.schema = joi.object({
--- src/helpers/server/repos/chatbots-repo.js
+++ src/helpers/server/repos/chatbots-repo.js
@@ -24,6 +24,7 @@ export const chatbotRepo = {
     updateLeadInfo,
     delete: _delete,
     updateModelData,
+    getChatbotSources,
     updateSecurityData,
     updateChatInterface,
     updateKnowledgebase,
@@ -113,8 +114,28 @@ async function trainChatbot(chatbotId) {
 }
 
 async function getById(id) {
+    if (!id || id === 'undefined') throw 'Please provide a valid bot id';
+  
+    const chatbot = await Chatbot.findById(id)
+      .select(
+        '+chatBotCustomizeData +owner +visibility +status +name +createdAt +updatedAt +rateLimiting +crawlData.charCount'
+      )
+      .lean();
+  
+    if (!chatbot) throw `Chatbot with id "${id}" not found`;
+  
+    const owner = headers().get('userId');
+    if (chatbot.owner.toString() !== owner) throw 'You do not own this chatbot';
+  
+    return {
+      ...chatbot,
+      charCount: chatbot.crawlData.charCount,
+    };
+  }
+
+async function getChatbotSources(id) {
     if (!id || id == 'undefined') throw 'Please provide a valid bot id'
-    const chatbot = await Chatbot.findById(id).select("+chatBotCustomizeData owner visibility status name createdAt updatedAt rateLimiting").lean();
+    const chatbot = await Chatbot.findById(id).select("+knowledgebase owner").lean();
     if (!chatbot) throw 'Chatbot with id "' + id + '"  not found';
 
     const owner = headers().get('userId');
