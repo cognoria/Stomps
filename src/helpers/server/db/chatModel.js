d3158382e87379dcc56529fcb3c83e96957b923f
Ikem <ikempeter2020@gmail.com>
2024-04-12 15:58:30 +0100

    chat analytic works

--- src/helpers/server/db/chatModel.js
+++ src/helpers/server/db/chatModel.js
@@ -10,9 +10,25 @@ export default function chatModel() {
         feedback: { type: String, enum: ChatAnswerFeedback }
     }, { _id: false, timestamps: true });
 
+    const userDataSchema = new Schema({
+        ip: String,
+        country: String,
+        countryCode: String,
+        region: String,
+        regionName: String,
+        city: String,
+        zip: String,
+        lat: Number,
+        lon: Number,
+        timezone: String,
+        isp: String,
+        org: String,
+        as: String
+    });
+
     const schema = new Schema({
         chatbot: { type: mongoose.Schema.Types.ObjectId, ref: 'Chatbot', required: true, },
-        userData: { type: mongoose.Schema.Types.Mixed, required: true, },
+        userData: { type: userDataSchema, required: true, },
         messages: [msg],
         isUnread: { type: Boolean, default: false }
     }, { timestamps: true });
