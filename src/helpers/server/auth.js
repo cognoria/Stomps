f17091cb980da5857a106f6b033b5941c5610247
Ikem <ikempeter2020@gmail.com>
2024-07-04 20:01:22 +0100

    auth fixes

--- src/helpers/server/auth.js
+++ src/helpers/server/auth.js
@@ -17,7 +17,8 @@ async function isAuthenticated() {
 }
 
 async function verifyToken() {
-    const token = cookies().get('authorization')?.value ?? headers().get('authorization').split(" ")[1];
+    const token = cookies().get('authorization')?.value || headers().get('authorization').split(" ")[1];
+    console.log({token})
     //TODO: JWT_Secret will be created randomly and saved for the user
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     // const decoded = jwt.verify(token, await globalRepo.getJwtSecret());
