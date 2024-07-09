a6170b87649a12394856d198110476f1e30b33ee
Ikem <ikempeter2020@gmail.com>
2024-07-09 16:41:42 +0100

    small update

--- src/helpers/server/auth.js
+++ src/helpers/server/auth.js
@@ -1,7 +1,5 @@
 import jwt from 'jsonwebtoken';
 import { cookies, headers } from 'next/headers';
-import { globalRepo } from './repos';
-import { cp } from 'fs';
 
 export const auth = {
     isAuthenticated,
@@ -19,8 +17,8 @@ async function isAuthenticated() {
 
 async function verifyToken() {
     const token = cookies().get('authorization')?.value || headers().get('Authorization');
-    console.log(cookies().get('authorization')?.value)
-    console.log(headers().get('Authorization'))
+    // console.log(cookies().get('authorization')?.value)
+    console.log(headers())
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     const id = decoded.sub;
     return id;
