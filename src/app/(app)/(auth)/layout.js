c3d14a2bed712a15b56562a05ed5e17ddd19747b
Ikem Peter <ikempeter2020@gmail.com>
2024-11-26 03:09:03 +0100

    updated licence check

--- src/app/(app)/(auth)/layout.js
+++ src/app/(app)/(auth)/layout.js
@@ -8,13 +8,15 @@ const inter = Inter({ subsets: ["latin"] });
 export default async function RootLayout({ children }) {
     const { INSTALLATION_KEY, PURCHASE_EMAIL, ADMIN_APP_CODE } = process.env;
     const adminAppUrl = process.env.NODE_ENV === 'production' ? 'https://app.stomps.io' : 'http://localhost:3000';
-    
+
     // Get the instance URL from headers
     const header = headers();
     const protocol = process.env.NODE_ENV === 'production' ? 'https' : 'http';
     const instanceUrl = `${protocol}://${header.get('host')}`;
-    
-    if ((ADMIN_APP_CODE !== 'stomps_admin_access_demo' && !adminAppUrl.includes('localhost')) || instanceUrl !== adminAppUrl) {
+
+    if (!instanceUrl.includes('localhost') &&
+        ADMIN_APP_CODE !== 'stomps_admin_access_demo' &&
+        instanceUrl !== adminAppUrl) {
         if (!INSTALLATION_KEY || !PURCHASE_EMAIL) {
             redirect('/licence-invalid');
         }
